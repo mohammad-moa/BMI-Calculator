fa6fcf70385b607c70f3a2c15bc307dc7ccc6f42
mohammad <aahrari5@gmail.com>
2025-05-31 13:56:30 +0330

    update font

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import { FC, Suspense } from "react";
 
 const App: FC = () => {
-  return (
-    <Suspense fallback={<>Loading ...</>}>
-      <h1 className="font-bold text-mint-500">سلام</h1>
-    </Suspense>
-  );
+  return <Suspense fallback={<>Loading ...</>}></Suspense>;
 };
 
 export default App;
