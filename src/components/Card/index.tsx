33c7c89b2f85df4ab84074b6d9416a169c0c620c
mohammad <aahrari5@gmail.com>
2025-08-22 14:29:49 +0330

    add card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,42 @@
+import { memo } from 'react'
+// components
+import { ColorType } from '@components/index.type'
+// utils
+import { makeClass } from '@utils/styles'
+// locals
+import { useClasses } from './useClasses'
+
+export type CardVariant = 'elevation' | 'outlined'
+
+export type CardProps = React.HTMLAttributes<HTMLDivElement> & {
+  variant?: CardVariant
+  color?: ColorType
+  rootClassName?: string
+}
+
+export const Card: React.FC<CardProps> = memo(
+  ({
+    children,
+    variant = 'elevation',
+    color = 'primary',
+    rootClassName,
+    ...props
+  }) => {
+    const className = useClasses()
+
+    return (
+      <div
+        className={makeClass(
+          className.card({
+            variant,
+            color,
+          }),
+          rootClassName
+        )}
+        {...props}
+      >
+        {children}
+      </div>
+    )
+  }
+)
