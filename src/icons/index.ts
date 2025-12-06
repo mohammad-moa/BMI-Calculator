006200e46d237508309633e81db0ae3bd0b639fb
mohammad <aahrari5@gmail.com>
2025-12-06 16:31:46 +0330

    add other icons

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,4 +1,9 @@
 import ChevronDown from './chevron-down.svg'
 import ChevronUp from './chevron-up.svg'
+import GenderFemale from './gender-female.svg'
+import GenderMale from './gender-male.svg'
+import Moon from './moon.svg'
+import Sun from './sun.svg'
+import User from './user.svg'
 
-export { ChevronDown, ChevronUp }
+export { ChevronDown, ChevronUp, GenderFemale, GenderMale, Moon, Sun, User }
