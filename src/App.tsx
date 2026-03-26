93f0ae36b3f382388ec1089c0a23334ba129cc0f
mohammad <aahrari5@gmail.com>
2026-03-26 10:26:16 +0330

    revert app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,7 @@
-import { Button, Dialog, Drawer } from '@components'
-import { FC, Suspense, useState } from 'react'
+import { FC, Suspense } from 'react'
 
 const App: FC = () => {
-  const [open, setOpen] = useState<'dialog' | 'drawer' | ''>('')
-  return (
-    <Suspense fallback={<>Loading ...</>}>
-      <Button onClick={() => setOpen('dialog')}>Open Dialog</Button>
-      <Button color='info' onClick={() => setOpen('drawer')}>
-        Open Drawer
-      </Button>
-      <Dialog open={open === 'dialog'} onClose={() => setOpen('')} header='Hi Dialog!'>
-        This is Dialog
-      </Dialog>
-      <Drawer
-        open={open === 'drawer'}
-        onClose={() => setOpen('')}
-        header='Hi Drawer!'
-        actions={{
-          confirm: {
-            children: 'confirm',
-            color: 'info',
-          },
-          cancel: {
-            children: 'cancel',
-            color: 'info',
-          },
-        }}
-      >
-        This is Drawer
-      </Drawer>
-    </Suspense>
-  )
+  return <Suspense fallback={<>Loading ...</>}></Suspense>
 }
 
 export default App
