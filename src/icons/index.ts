8152074c7a2e3c9a99ecd407cf15a9ada3da81e0
mohammad <aahrari5@gmail.com>
2026-06-11 08:13:13 +0330

    add use click outside hook and update ts config

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,9 +1,10 @@
 import ChevronDown from './chevron-down.svg'
 import ChevronUp from './chevron-up.svg'
+import Close from './close.svg'
 import GenderFemale from './gender-female.svg'
 import GenderMale from './gender-male.svg'
 import Moon from './moon.svg'
 import Sun from './sun.svg'
 import User from './user.svg'
 
-export { ChevronDown, ChevronUp, GenderFemale, GenderMale, Moon, Sun, User }
+export { ChevronDown, ChevronUp, Close, GenderFemale, GenderMale, Moon, Sun, User }
