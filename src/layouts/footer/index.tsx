642d2de387d8c967a8ee048f147adfea6670c15b
mohammad <aahrari5@gmail.com>
2026-06-11 08:25:59 +0330

    add init layout files

---
+++ src/layouts/footer/index.tsx
@@ -0,0 +1,10 @@
+// locals
+import { useClasses } from './useClasses'
+
+type FooterProps = {}
+
+export const Footer: React.FC<FooterProps> = () => {
+  const className = useClasses()
+
+  return <></>
+}
---
+++ src/layouts/header/index.tsx
@@ -0,0 +1,10 @@
+// locals
+import { useClasses } from './useClasses'
+
+type HeaderProps = {}
+
+export const Header: React.FC<HeaderProps> = () => {
+  const className = useClasses()
+
+  return <></>
+}
