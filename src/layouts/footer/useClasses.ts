f24b19d004f58863aca34559fd84ba987627ff17
mohammad <aahrari5@gmail.com>
2026-06-13 10:08:12 +0330

    update vite project

--- src/layouts/footer/useClasses.ts
+++ src/layouts/footer/useClasses.ts
@@ -2,7 +2,7 @@ import { cva } from 'class-variance-authority'
 
 export const useClasses = () => {
   return {
-    root: cva('flex items-center justify-center gap-4 py-3'),
-    copyright: cva('text-gray-400 text-sm'),
+    root: cva('flex items-center justify-center gap-4 pb-3 pt-3.5 bg-white my-5 rounded-2xl'),
+    copyright: cva('text-slate-400 text-sm'),
   }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,4 @@ export default defineConfig({
       include: '**/*.svg',
     }),
   ],
-  css: {
-    devSourcemap: false,
-  },
 })
