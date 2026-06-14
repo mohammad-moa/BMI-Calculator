b6802cd7fe01db1071eda77d78fac1d07998b2a3
mohammad <aahrari5@gmail.com>
2026-06-14 10:47:57 +0330

    update home page with element and form

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,5 @@
 // images
-import BmiChart from '@assets/images/bmi-chart.jpg'
+import BmiChart from '@assets/images/bmi-chart.png'
 // components
 import { Button, TextField } from '@components'
 // hooks
@@ -13,19 +13,18 @@ export const HomePage: React.FC<HomePageProps> = () => {
   const className = useClasses()
   const { TX } = useText()
 
-  return (
-    <div className={className.root()}>
-      <div className={className.form()}>
-        <h1 className={className.title()}>BMI Calculator</h1>
-        <TextField fullWidth label={TX('HOME.AGE')} color='primary' />
+  const renderForm = () => {
+    return (
+      <>
+        <TextField fullWidth type='number' label={TX('HOME.AGE')} color='primary' />
         <TextField
           fullWidth
           label={TX('HOME.WEIGHT')}
           color='primary'
           endIcon={
             <div className={className.units()}>
-              <span className={className.unit()}>Kg</span>
-              <span className={className.unit()}>Lb</span>
+              <span className={className.unit()}>{TX('HOME.KG')}</span>
+              <span className={className.unit()}>{TX('HOME.LB')}</span>
             </div>
           }
         />
@@ -35,12 +34,21 @@ export const HomePage: React.FC<HomePageProps> = () => {
           color='primary'
           endIcon={
             <div className={className.units()}>
-              <span className={className.unit()}>Cm</span>
-              <span className={className.unit()}>Ft</span>
+              <span className={className.unit()}>{TX('HOME.CM')}</span>
+              <span className={className.unit()}>{TX('HOME.FT')}</span>
             </div>
           }
         />
         <Button>{TX('HOME.CALCULATE')}</Button>
+      </>
+    )
+  }
+
+  return (
+    <div className={className.root()}>
+      <div className={className.form()}>
+        <h1 className={className.title()}>{TX('HOME.BMI_CALCULATOR')}</h1>
+        {renderForm()}
       </div>
       <div className={className.image()}>
         <img src={BmiChart} alt='bmi-chart' />
--- src/pages/home/useClasses.ts
+++ src/pages/home/useClasses.ts
@@ -2,11 +2,11 @@ import { cva } from 'class-variance-authority'
 
 export const useClasses = () => {
   return {
-    root: cva('flex flex-1 items-center gap-15'),
+    root: cva('flex flex-1 items-center justify-between gap-15'),
     form: cva('flex flex-col gap-8'),
     title: cva('text-5xl font-black'),
     units: cva('flex items-center gap-3 border-l border-l-gray-300 pl-3'),
-    unit: cva('cursor-pointer text-blue-500'),
+    unit: cva('cursor-pointer text-gray-700'),
     image: cva('w-3/5'),
   }
 }
